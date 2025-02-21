bc4375c64c28f2663ffa34d93a9eb3d3d320429d
James Chase <gochases@gmail.com>
2025-02-20 23:08:26 -0700

    Add post endpoint for Add Transaction
    Allows transaction to be added
    to the database via API.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 // imports
 const express = require('express');
 const dotenv = require('dotenv');
-const { getCurrentMonth } = require('./utils/getCurrentMonth');
+const { getStringMonth } = require('./utils/getStringMonth');
 
 // main app
 const app = express();
@@ -26,6 +26,7 @@ app.set('views', 'templates');
 // middleware & static files
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // home page GET
 app.get('/', (_, res) => {
@@ -41,7 +42,7 @@ app.get('/', (_, res) => {
             user = { name: data.name, id: data.id };
             transactions = (data.transactions.length >= 4) ? data.transactions.slice(-3) : data.transactions;
             res.status(200).render('home', {
-                month: getCurrentMonth(),
+                month: getStringMonth(new Date().getMonth()),
                 chart: "chart.jpg",
                 user: user,
                 transactions: transactions
@@ -80,6 +81,35 @@ app.get('/charts', (_, res) => {
     ]
 
     res.status(200).render('charts', { charts: stubCharts });
+});
+
+/*
+    POST ENDPOINTS
+*/
+
+app.post('/api/add', (req, res) => {
+    const date = new Date(req.body.date);
+    fetch(`http://${process.env.API_HOST}/transaction/add`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: null,
+            date: `${date.getDate() + 1}-${getStringMonth(date.getMonth()).slice(0, 3)}-${date.getFullYear()}`,
+            description: req.body.description,
+            category: req.body.category,
+            amount: req.body.amount
+        })
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to add transaction');
+            }
+            return response.text();
+        })
+        .then(data => res.status(200).send(data))
+        .catch(error => console.error(error));
 });
 
 // 404 page
