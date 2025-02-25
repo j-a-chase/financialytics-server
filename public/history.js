e1050d6a80102c544f48de4f53e3c569de87f0b8
James Chase <gochases@gmail.com>
2025-02-24 20:15:32 -0700

    Update helpers in history.js
    Move helpers to bottom to prioritze
    functional code.
    
    Add helper for categories to ease
    modification of the available categories.

--- public/history.js
+++ public/history.js
@@ -1,3 +1,78 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const addTransaction = document.querySelector('button');
+    addTransaction.addEventListener('click', () => {
+        addTransaction.disabled = true;
+        let tbody = document.querySelector('table').querySelector('tbody');
+        let newTransaction = document.createElement('tr');
+        newTransaction.innerHTML = `
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        `;
+        let dateInput = document.createElement('input')
+        dateInput.setAttribute('type', 'date');
+        let descriptionInput = document.createElement('input')
+        descriptionInput.setAttribute('type', 'text');
+        let categoryInput = document.createElement('select');
+        categoryInput.innerHTML = getCategoryString();
+        let amountInput = document.createElement('input');
+        amountInput.setAttribute('type', 'number');
+        amountInput.setAttribute('step', '.01');
+        amountInput.setAttribute('min', '0');
+        const saveButton = createSaveButton(dateInput, descriptionInput, categoryInput, amountInput);
+
+        let tableData = newTransaction.querySelectorAll('td');
+        tableData[0].appendChild(dateInput);
+        tableData[1].appendChild(descriptionInput);
+        tableData[2].appendChild(categoryInput);
+        tableData[3].appendChild(amountInput);
+        tableData[4].appendChild(saveButton);
+        tbody.appendChild(newTransaction);
+    });
+
+    const editButtons = document.querySelectorAll('.edit-button');
+    editButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            const tr = event.target.closest('tr');
+            const rowChildren = tr.children;
+            const date = rowChildren[0].innerHTML;
+            const description = rowChildren[1].innerHTML;
+            const category = rowChildren[2].innerHTML;
+            const amount = parseFloat(rowChildren[3].innerHTML.slice(1)).toFixed(2);
+
+            let dateInput = document.createElement('input');
+            dateInput.setAttribute('type', 'date');
+            dateInput.value = `${date.split('-')[2]}-${getMonthNumber(date.split('-')[1])}-${date.split('-')[0]}`;
+            let descriptionInput = document.createElement('input');
+            descriptionInput.setAttribute('type', 'text');
+            descriptionInput.value = description;
+            let categoryInput = document.createElement('select');
+            categoryInput.innerHTML = getCategoryString();
+            categoryInput.value = category.toLowerCase();
+            let amountInput = document.createElement('input');
+            amountInput.setAttribute('type', 'number');
+            amountInput.setAttribute('step', '.01');
+            amountInput.setAttribute('min', '0');
+            amountInput.value = amount;
+            const saveButton = createSaveButton(dateInput, descriptionInput, categoryInput, amountInput, false, tr);
+
+            rowChildren[0].innerHTML = '';
+            rowChildren[1].innerHTML = '';
+            rowChildren[2].innerHTML = '';
+            rowChildren[3].innerHTML = '';
+            rowChildren[4].innerHTML = '';
+
+            rowChildren[0].appendChild(dateInput);
+            rowChildren[1].appendChild(descriptionInput);
+            rowChildren[2].appendChild(categoryInput);
+            rowChildren[3].appendChild(amountInput);
+            rowChildren[4].appendChild(saveButton);
+        });
+    });
+});
+
 createSaveButton = (
     dateInput, descriptionInput, categoryInput,
     amountInput, isAdding=true, tableRow=null
@@ -62,91 +137,14 @@ getMonthNumber = (month) => {
     }
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-    const addTransaction = document.querySelector('button');
-    addTransaction.addEventListener('click', () => {
-        addTransaction.disabled = true;
-        let tbody = document.querySelector('table').querySelector('tbody');
-        let newTransaction = document.createElement('tr');
-        newTransaction.innerHTML = `
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        `;
-        let dateInput = document.createElement('input')
-        dateInput.setAttribute('type', 'date');
-        let descriptionInput = document.createElement('input')
-        descriptionInput.setAttribute('type', 'text');
-        let categoryInput = document.createElement('select');
-        categoryInput.innerHTML = `
-            <option value="food">Food</option>
-            <option value="living">Living</option>
-            <option value="entertainment">Entertainment</option>
-            <option value="supplies">Supplies</option>
-            <option value="education">Education</option>
-            <option value="other">Other</option>
-        `;
-        let amountInput = document.createElement('input');
-        amountInput.setAttribute('type', 'number');
-        amountInput.setAttribute('step', '.01');
-        amountInput.setAttribute('min', '0');
-        const saveButton = createSaveButton(dateInput, descriptionInput, categoryInput, amountInput);
-
-        let tableData = newTransaction.querySelectorAll('td');
-        tableData[0].appendChild(dateInput);
-        tableData[1].appendChild(descriptionInput);
-        tableData[2].appendChild(categoryInput);
-        tableData[3].appendChild(amountInput);
-        tableData[4].appendChild(saveButton);
-        tbody.appendChild(newTransaction);
-    });
-
-    const editButtons = document.querySelectorAll('.edit-button');
-    editButtons.forEach(button => {
-        button.addEventListener('click', (event) => {
-            const tr = event.target.closest('tr');
-            const rowChildren = tr.children;
-            const date = rowChildren[0].innerHTML;
-            const description = rowChildren[1].innerHTML;
-            const category = rowChildren[2].innerHTML;
-            const amount = parseFloat(rowChildren[3].innerHTML.slice(1)).toFixed(2);
-
-            let dateInput = document.createElement('input');
-            dateInput.setAttribute('type', 'date');
-            dateInput.value = `${date.split('-')[2]}-${getMonthNumber(date.split('-')[1])}-${date.split('-')[0]}`;
-            let descriptionInput = document.createElement('input');
-            descriptionInput.setAttribute('type', 'text');
-            descriptionInput.value = description;
-            let categoryInput = document.createElement('select');
-            categoryInput.innerHTML = `
-                <option value="food">Food</option>
-                <option value="living">Living</option>
-                <option value="entertainment">Entertainment</option>
-                <option value="supplies">Supplies</option>
-                <option value="education">Education</option>
-                <option value="other">Other</option>
-            `;
-            categoryInput.value = category.toLowerCase();
-            let amountInput = document.createElement('input');
-            amountInput.setAttribute('type', 'number');
-            amountInput.setAttribute('step', '.01');
-            amountInput.setAttribute('min', '0');
-            amountInput.value = amount;
-            const saveButton = createSaveButton(dateInput, descriptionInput, categoryInput, amountInput, false, tr);
-
-            rowChildren[0].innerHTML = '';
-            rowChildren[1].innerHTML = '';
-            rowChildren[2].innerHTML = '';
-            rowChildren[3].innerHTML = '';
-            rowChildren[4].innerHTML = '';
-
-            rowChildren[0].appendChild(dateInput);
-            rowChildren[1].appendChild(descriptionInput);
-            rowChildren[2].appendChild(categoryInput);
-            rowChildren[3].appendChild(amountInput);
-            rowChildren[4].appendChild(saveButton);
-        });
-    });
-});
+getCategoryString = () => {
+    return `
+        <option value="income">Income</option>
+        <option value="food">Food</option>
+        <option value="living">Living</option>
+        <option value="entertainment">Entertainment</option>
+        <option value="supplies">Supplies</option>
+        <option value="education">Education</option>
+        <option value="other">Other</option>
+    `;
+}
